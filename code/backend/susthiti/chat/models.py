2df839e0cef0d30455026e0630589b570e14a28c
dilli <dillihangrae@gmail.com>
2024-06-12 17:45:46 +0545

    dilli [added] wsgi and chat systemtesting....

---
+++ code/backend/susthiti/chat/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class Message(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.content[:20]}'
