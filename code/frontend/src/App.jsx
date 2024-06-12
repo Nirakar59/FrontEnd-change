a56c10e50cc1a885d56d9f4db65b1a08ed33cd38
aabhushanCD <dammardhakal426@gmail.com>
2024-06-12 21:19:11 +0545

    aabhushan{created engadgements, quiz and breathing excercise}

--- code/frontend/src/App.jsx
+++ code/frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import "./App.css";
-import ChatComponent from "./Component/chat";
+import MyComponent from "./Component/chat";
 import RegistrationForm from "./Component/registration/userRegister";
+import BreathingExercise from "./Component/Engadgement/engadgement";
+import Quiz from "./Component/Engadgement/quizGame";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       {/* <ChatComponent></ChatComponent> */}
-      <RegistrationForm></RegistrationForm>
+      {/* <RegistrationForm></RegistrationForm> */}
+      {/* <MyComponent></MyComponent> */}
+
+      <BreathingExercise></BreathingExercise>
+      <Quiz></Quiz>
     </>
   );
 }
